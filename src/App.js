b0f0486056746256f32e9254ad5ca39f6936a6ba
Gustavo Araujo <gustavoaraujo600@hotmail.com>
2020-07-06 14:03:53 -0300

    initial

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import GlobalStyle from './styles/global';
+
+function App() {
+  return (
+    <>
+      <h1>Hello Bowe Tech</h1>
+      <GlobalStyle />
+    </>
+  );
+}
+
+export default App;
